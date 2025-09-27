3a5c7780dd23d4bcc30e9b8ced032d2073ad7d12
Kais A Saradi <kais.saradi@gmail.com>
2025-09-26 22:52:37 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+from qtpy.QtWidgets import QApplication
+from gui.main_window import MainWindow
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
